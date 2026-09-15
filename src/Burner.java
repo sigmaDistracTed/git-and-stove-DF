76174a4a8cf9254dac751cbf4f61b6ea2fc2d540
Jeremiah Durham <jeremiah_durham@mines.edu>
2026-09-15 15:17:35 -0600

    Created button functions

--- src/Burner.java
+++ src/Burner.java
@@ -14,7 +14,51 @@ public class Burner {
 	}
 	
 	
+	public Burner() {
+		this.myTemperature = Temperature.COLD;
+		this.mySetting = Setting.OFF;
+	}
+	
+	public void plusButton() {
+		switch(this.mySetting) {
+			case OFF:
+				this.mySetting = Setting.LOW;
+				break;
+			case LOW:
+				this.mySetting = Setting.MEDIUM;
+				break;
+			case MEDIUM:
+				this.mySetting = Setting.HIGH;
+				break;
+			case HIGH:
+				break;
+		}
+		
+		this.timer = TIME_DURATION;
+	}
 
+	public void minusButton() {
+		switch(this.mySetting) {
+			case OFF:
+				break;
+			case LOW:
+				this.mySetting = Setting.OFF;
+				break;
+			case MEDIUM:
+				this.mySetting = Setting.LOW;
+				break;
+			case HIGH:
+				this.mySetting = Setting.MEDIUM;
+				break;
+		}
+		
+		this.timer = TIME_DURATION;
+	}
+	
+	
+	public void updateTemperature() {
+		
+	}
 }
 
 
